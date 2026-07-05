4967963d9b81ea566784b15351fd2a1b964bf73c
gabrielaguedes12 <gabr1.profissional@gmail.com>
2026-07-04 21:24:41 -0300

    segue o projetinho

--- Iniciante/ToDoList/ToDoList.py
+++ Iniciante/ToDoList/ToDoList.py
@@ -22,4 +22,37 @@ class TodoApp:
          )   
         title_lable.pack(pady=10)
         
+        #Cria um Frame para agrupar o campo de texto e o botão de adicionar lado a lado.
+        input_frame = tk.Frame(self.root, bg="#f0f4f7")
+        input_frame.pack(pady=10)
         
+        #Entry é o widget de campo de texto — onde o usuário digita.
+        self.task_entry = tk.Entry(
+            input_frame,
+            font = ("Helvetica", 12),
+            width=30
+        )
+        
+        self.task_entry.pack(side="left", padx=(0,10))
+        
+        add_button = tk.Button(
+            input_frame,
+            text= "Adicione tarefa",
+            font=("Helvetica", 11, "bold"),
+            bg= "#27ae60",
+            fg="white",
+            padx=10,
+            command= self.add_task
+        )        
+        add_button.pack(side="left")
+        
+        list_frame = tk.Frame(self.root, bg="#f0f4f7")
+        list_frame.pack(pady=10, fill="both", expand=True)
+        
+        self.task_listbox = tk.Listbox(
+            list_frame,
+            font=("Heveltica", 12),
+            width= 45,
+            height=10,
+            activestyle="none"
+        )
